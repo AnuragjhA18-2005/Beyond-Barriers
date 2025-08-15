5701fa239b25e9c92a8445d3bd55df42283fc70d
ARYANROY20 <aryanroy200606@gmail.com>
2025-08-15 13:25:32 +0530

    Sticky nav bar

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ const menuToggle = document.getElementById('menu-toggle');
     menuToggle.classList.toggle('active');
     navLinks.classList.toggle('active');
   });
+
+
+
+// To change the style of nav on scrolling
+const navbar = document.querySelector('header');
+const navLinks1 = document.getElementById('nav-links');
+const logo = document.querySelector('.logo');
+window.addEventListener('scroll', () => {
+    if (window.pageYOffset>navLinks1.offsetTop) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
